ce32ceaa9e7bad46fca533e000f1bc7e39b38f09
KoriDavid <mutugidavid37@gmail.com>
2026-02-05 23:38:14 +1100

    change the axios link to production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
-// const API_BASE_URL = 'https://portfolio-backend-xvu9.onrender.com/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://portfolio-backend-xvu9.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
